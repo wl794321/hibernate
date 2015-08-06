cc2a5ba032eccc154743f06eaa8a7eadc9a2b789
wl794321 <1044913154@qq.com>
2015-08-06 17:04:12 +0800

    frist commit

---
+++ src/main/java/entry/Student.java
@@ -0,0 +1,45 @@
+package entry;
+
+/**
+ * Created by Mr.Wang on 2015/8/4.
+ */
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    public Student(){
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
